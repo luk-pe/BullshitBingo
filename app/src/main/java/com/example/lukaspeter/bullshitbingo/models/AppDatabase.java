de6a1a0ed478d6b7f42e18b9bf76bf206c883181
Lukas Peter <peterl@dhbw-loerrach.de>
2019-01-25 18:15:51 +0100

    Type Converters added (room cant deal with date format)

--- app/src/main/java/com/example/lukaspeter/bullshitbingo/models/AppDatabase.java
+++ app/src/main/java/com/example/lukaspeter/bullshitbingo/models/AppDatabase.java
@@ -2,8 +2,12 @@ package com.example.lukaspeter.bullshitbingo.models;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+
+import com.example.lukaspeter.bullshitbingo.helpers.Converters;
 
 @Database(entities = {Game.class, Gamelog.class, Item.class, Template.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract GameDao gameDao();
     public abstract GamelogDao gamelogDao();
